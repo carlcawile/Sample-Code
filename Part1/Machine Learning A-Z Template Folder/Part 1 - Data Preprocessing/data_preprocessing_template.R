026c9c54cc2ed67c20dfa00e584832b4cdf3a2cb
carlcawile <carlcawile@gmail.com>
2020-03-19 11:31:25 +0800

    Multiple Linear Regression

--- Part1/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part1/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -3,13 +3,22 @@
 # Importing the dataset
 dataset = read.csv('Data.csv')
 
+
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+#library(caTools)
+#set.seed(123)
+#split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+#training_set = subset(dataset, split == TRUE)
+#test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
